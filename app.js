7ee3eaad0d3dc7449741d3d7d89ddf3063eeace9
Prince Reyes <krayzerprince@gmail.com>
2023-06-24 21:53:46 +0800

    applying css challenge 3

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+const homeStartingContent =
+  "Occaecat id nulla do excepteur nisi enim. Excepteur elit elit ea esse excepteur. Cillum id ipsum quis esse culpa amet duis amet nisi aliquip est dolor nostrud velit.";
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    const hello = "hello world!";
-    res.render("home", {fromServer: hello});
+  res.render("home", { startingContent: homeStartingContent });
 });
 
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
